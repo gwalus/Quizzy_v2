2ea7ca2341d9f5538414cde522d75769c3884440
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-05 15:43:42 +0200

    Add default connection string to local postgres server

--- API/Data/QuizContext.cs
+++ API/Data/QuizContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     public class QuizContext : IdentityDbContext
     {
-
+        public QuizContext(DbContextOptions options) : base(options)
+        {
+        }
     }
 }
