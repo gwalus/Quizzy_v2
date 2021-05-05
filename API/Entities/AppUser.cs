9f40b63d1df9098f5041d19fa16315d8db59dfed
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-05 15:48:53 +0200

    Add AppUser entity model

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace API.Entities
+{
+    public class AppUser : IdentityUser<int>
+    {
+        public DateTime Created { get; set; } = DateTime.Now;
+        public string FavouriteCategory { get; set; }
+    }
+}
