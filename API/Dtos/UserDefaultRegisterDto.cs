daa4868a1250ff0f115bfa8aef0e3db027d05fc7
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-05 17:51:42 +0200

    Add UserDefaultRegisterDto class for default registration

---
+++ API/Dtos/UserDefaultRegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Dtos
+{
+    public class UserDefaultRegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FavouriteCategory { get; set; }
+    }
+}
