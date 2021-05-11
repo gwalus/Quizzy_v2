325e083936e304524e445cb28f977872b406f56f
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-11 17:20:28 +0200

    Load categories in home component

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TriviaService } from '../_services/trivia.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  categories: any = {};
 
-  constructor() { }
+  constructor(private triviaService: TriviaService) { }
 
   ngOnInit(): void {
+    this.loadCategories();
   }
 
+  loadCategories() {
+    this.triviaService.getCategories().subscribe(response => {
+      this.categories = response;
+      console.log(response);
+    }
+    )
+  }
 }
