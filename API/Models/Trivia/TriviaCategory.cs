b20ba5e1ee074ab2ad29211a7aefb0aac4edf0a3
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-11 11:37:42 +0200

    Add Trivia Category response model

---
+++ API/Models/Trivia/TriviaCategory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace API.Models.Trivia
+{
+    public class TriviaCategory
+    {
+        public IList<Category> Trivia_categories { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
