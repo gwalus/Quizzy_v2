513021939869293be92941ccd43d1d345082389f
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-11 12:02:06 +0200

    Add ITrivia service

---
+++ API/Interfaces/ITriviaService.cs
@@ -0,0 +1,14 @@
+﻿using API.Dtos;
+using API.Models.Trivia;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Interfaces
+{
+    interface ITriviaService
+    {
+        Task<IList<Result>> GetQuestions(string category, string difficulty, string amount, string type);
+        Task<IList<TriviaCategoryDto>> GetCategories();
+        Task<CategoryQuestionCount> GetCategoriesQuantity(string categoryId);
+    }
+}
