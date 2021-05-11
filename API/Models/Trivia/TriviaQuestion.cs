43d5e842f4e659bd42a9c9cf79b30b45e9993b23
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-11 11:36:18 +0200

    Add Trivia Question response model

---
+++ API/Models/Trivia/TriviaQuestion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace API.Models.Trivia
+{
+    public class TriviaQuestion
+    {
+        public int Response_code { get; set; }
+        public IList<Result> Results { get; set; }
+    }
+
+    public class Result
+    {
+        public string Category { get; set; }
+        public string Type { get; set; }
+        public string Difficulty { get; set; }
+        public string Question { get; set; }
+        public string Correct_answer { get; set; }
+        public IList<string> Incorrect_answers { get; set; }
+    }
+}
