32448b1d548c8a45511f27840ce50351ece23f98
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-12 10:42:49 +0200

    Fix bug with rendering random colors

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { interval } from 'rxjs';
 import { Category } from '../_models/category';
 import { TriviaService } from '../_services/trivia.service';
 
@@ -14,14 +15,15 @@ export class HomeComponent implements OnInit {
   colorTypes: string[] = [
     'primary', 'secondary', 'success', 'danger', 'warning', 'info', 'dark'
   ];
+  randomTypes: string[] = [];
   choosedCategory?: string;
 
   constructor(private triviaService: TriviaService, private toastr: ToastrService, private router: Router) {
-    this.loadCategories();
   }
 
   ngOnInit(): void {
-
+    this.loadCategories();
+    this.createRandomColorsTable();
   }
 
   loadCategories() {
@@ -30,15 +32,28 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  getRandomBtnColor() {
-    return this.colorTypes[Math.floor(Math.random() * this.colorTypes.length)];
+  getRandomBtnColor(num: string) {
+    let temp = Number.parseInt(num);
+    temp -= 9;
+    return this.randomTypes[temp];
+  }
+
+  createRandomColorsTable() {
+    let table: string[] = [];
+
+    for (let i = 0; i < 24; i++) {
+      let randomColor = this.colorTypes[Math.floor(Math.random() * this.colorTypes.length)];
+      table.push(randomColor);
+    }
+    this.randomTypes = table;
+
   }
 
   chooseCategory(categoryId: string) {
     this.choosedCategory = categoryId;
   }
 
-  StartQuiz() {
+  startQuiz() {
     if (this.choosedCategory)
       this.router.navigateByUrl('game');
     else this.toastr.error('You have not selected any category.');
