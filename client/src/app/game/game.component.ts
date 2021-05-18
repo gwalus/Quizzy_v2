f95e5f645c92db123dd7a2fcb10d4166b3e7ffcf
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-18 11:55:38 +0200

    Shuffle answers funtion

--- client/src/app/game/game.component.ts
+++ client/src/app/game/game.component.ts
@@ -44,8 +44,31 @@ export class GameComponent implements OnInit {
     console.log(question)
 
     let answers = question.incorrect_answers;
-    answers.push(question.correct_answer)
+    answers.push(question.correct_answer)    
     console.log(answers);
+
+    answers = this.shuffleAnswers(answers)
+
+    console.log(answers);
+  }
+
+  shuffleAnswers(array: string[]) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
   }
 
 }
