aabb3e1ce92965bbf417a8487f915b291b25959f
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-18 13:16:48 +0200

    Default display answers

--- client/src/app/game/game.component.ts
+++ client/src/app/game/game.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
 import { Question } from '../_models/question';
 import { TriviaService } from '../_services/trivia.service';
 
@@ -9,10 +8,12 @@ import { TriviaService } from '../_services/trivia.service';
   styleUrls: ['./game.component.css'],
 })
 export class GameComponent implements OnInit {
+  loading = true;
   questions: Question[] = [];
   currentQuestion: Question;
   currentQuestionNumber: number = 0;
   currentAnswers: string[];
+  currentCorrectAnswer: string;
 
   strange: string = 'Where is the train station &quot;Llanfair&shy;pwllgwyngyll&shy;gogery&shy;chwyrn&shy;drobwll&shy;llan&shy;tysilio&shy;gogo&shy;goch&quot;?'
 
@@ -30,6 +31,7 @@ export class GameComponent implements OnInit {
 
         this.setCurrentQuestion(currentQuestionNumber)
         this.setCurrentAnswers()
+        this.loading = false;
       },
       error => console.log(error.error)
     );
@@ -44,30 +46,32 @@ export class GameComponent implements OnInit {
     console.log(question)
 
     let answers = question.incorrect_answers;
-    answers.push(question.correct_answer)    
+    answers.push(question.correct_answer)
     console.log(answers);
 
     answers = this.shuffleAnswers(answers)
 
-    console.log(answers);
+    this.currentCorrectAnswer = question.correct_answer;
+    console.log(this.currentCorrectAnswer)
+    this.currentAnswers = answers;
   }
 
   shuffleAnswers(array: string[]) {
     let currentIndex = array.length, temporaryValue, randomIndex;
-  
+
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-  
+
       // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
-  
+
       // And swap it with the current element.
       temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
       array[randomIndex] = temporaryValue;
     }
-  
+
     return array;
   }
 
