536befe14cb762f44562e626587a1d8eadb99527
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-21 12:51:17 +0200

    Login method functionality

--- client/src/app/_services/auth.service.ts
+++ client/src/app/_services/auth.service.ts
@@ -27,6 +27,17 @@ export class AuthService {
     )
   }
 
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response: any) => {
+        const user = response;
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
+    )
+  }
+
   setCurrentUser(user: any) {
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
