d03d5a180baae781e871eaaa274bd79864302288
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-21 13:46:23 +0200

    Login handling

--- client/src/app/_services/auth.service.ts
+++ client/src/app/_services/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +16,7 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-  register(model: any) {
+  register(model: User) {
     return this.http.post(this.baseUrl + 'register', model).pipe(
       map((user: any) => {
         if (user) {
@@ -27,8 +28,8 @@ export class AuthService {
     )
   }
 
-  login(model: any) {
-    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+  login(model: User) {
+    return this.http.post(this.baseUrl + 'login', model).pipe(
       map((response: any) => {
         const user = response;
         if (user) {
@@ -38,7 +39,7 @@ export class AuthService {
     )
   }
 
-  setCurrentUser(user: any) {
+  setCurrentUser(user: User) {
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AuthService } from '../_services/auth.service';
 
 @Component({
@@ -9,13 +10,17 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   login() {
-
+    this.authService.login(this.model).subscribe(() => {
+      this.toastr.success('Login successfully');
+    }, error => {
+      this.toastr.error(error.error);
+    })
   }
 
 }
