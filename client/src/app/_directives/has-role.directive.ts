427c0bcd01aaf98fea25a7fed607e6ee4095b65a
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-21 14:47:12 +0200

    Implementation has role directive

--- client/src/app/_directives/has-role.directive.ts
+++ client/src/app/_directives/has-role.directive.ts
@@ -1,10 +1,31 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
+import { take } from 'rxjs/operators';
+import { User } from '../_models/user';
+import { AuthService } from '../_services/auth.service';
 
 @Directive({
   selector: '[appHasRole]'
 })
-export class HasRoleDirective {
+export class HasRoleDirective implements OnInit {
+  @Input() appHasRole: string[];
+  user: User;
 
-  constructor() { }
+  constructor(private viewContainerRef: ViewContainerRef, private templateRef: TemplateRef<any>, private authService: AuthService) {
+    this.authService.currentUser$.pipe(take(1)).subscribe(user => {
+      this.user = user as User;
+    })
+  }
 
+  ngOnInit(): void {
+    if (!this.user?.roles || this.user == null) {
+      this.viewContainerRef.clear();
+      return;
+    }
+
+    if (this.user?.roles.some(r => this.appHasRole.includes(r))) {
+      this.viewContainerRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.viewContainerRef.clear();
+    }
+  }
 }
