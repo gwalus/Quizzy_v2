e5b76730654d548611de5fc9f1dd571cc50a4d48
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-21 14:56:03 +0200

    Set user roles in set current user method for auth service

--- client/src/app/_guards/admin.guard.ts
+++ client/src/app/_guards/admin.guard.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../_models/user';
+import { AuthService } from '../_services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(private authService: AuthService, private toastr: ToastrService) { }
+
+  canActivate(): Observable<boolean> {
+    return this.authService.currentUser$.pipe(
+      map(user => {
+        if ((user as User)?.roles.includes('Admin') || (user as User)?.roles.includes('Moderator')) {
+          return true;
+        }
+        this.toastr.error('You cannot enter this area');
+        return false;
+      })
+    )
   }
-  
+
 }
