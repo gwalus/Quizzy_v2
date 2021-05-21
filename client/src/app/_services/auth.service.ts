e74c6d8426bd8c1f1d998153fcd151918e1253d9
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-21 12:49:47 +0200

    Add register method in auth service

--- client/src/app/_services/auth.service.ts
+++ client/src/app/_services/auth.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  baseUrl = environment.baseUrl + 'auth/';
+  private currentUserSource = new ReplaySubject(1);
+  // przechowujemy tylko jedno
+  currentUser$ = this.currentUserSource.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model).pipe(
+      map((user: any) => {
+        if (user) {
+          this.setCurrentUser(user);
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
 }
