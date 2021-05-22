eb4a8a4df95173fa97e087fe38d4379180e4f85c
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-22 11:55:01 +0200

    Base form for adding new category

--- client/src/app/admin/admin.component.ts
+++ client/src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  categoryForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.categoryForm = this.fb.group({
+      category: ['', Validators.required]
+    });
+  }
+
+  AddCategory() {
+    console.log(this.categoryForm.value);
   }
 
 }
