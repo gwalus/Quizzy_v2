d3c5a65d95ff072d1b8823420e0b7781dc936433
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-23 14:36:33 +0200

    Add get categories in question service

---
+++ API/Controllers/QuestionController.cs
@@ -0,0 +1,10 @@
+namespace API.Controllers
+{
+    public class QuestionController : BaseApiController
+    {
+        public QuestionController()
+        {
+
+        }
+    }
+}
--- API/Services/QuestionService.cs
+++ API/Services/QuestionService.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Dtos;
@@ -47,6 +48,11 @@ namespace API.Services
         public async Task<bool> CategoryExists(string category)
         {
             return await _context.CustomCategories.AnyAsync(c => c.Name == category);
+        }
+
+        public async Task<IList<string>> GetCategories()
+        {
+            return await _context.CustomCategories.Select(c => c.Name).ToListAsync();
         }
     }
 }
