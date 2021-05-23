aa4e2012c94f42eff1de562978e0c92713b249b7
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-23 14:38:15 +0200

    Get categories from database endpoint

--- API/Controllers/QuestionController.cs
+++ API/Controllers/QuestionController.cs
@@ -1,10 +1,22 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
 namespace API.Controllers
 {
     public class QuestionController : BaseApiController
     {
-        public QuestionController()
+        private readonly IQuestionService _questionService;
+        public QuestionController(IQuestionService questionService)
         {
+            _questionService = questionService;
+        }
 
+        [HttpGet("categories")]
+        public async Task<IList<string>> GetCategories()
+        {
+            return await _questionService.GetCategories();
         }
     }
 }
